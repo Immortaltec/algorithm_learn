657a424fbbba97ff940cb22bbf3e56b0ac56ea7e
Kyle <95911915+Immortaltec@users.noreply.github.com>
2022-05-10 09:34:39 +0800

    Update 5.顺序数组.cpp

--- data_structure/Basic_data_structure/5.顺序数组.cpp
+++ data_structure/Basic_data_structure/5.顺序数组.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
+#include<stdarg.h>
 using namespace std;
 
-typedef char AtomType;
-typedef enum{ATOM,LIST}ElemTag;
-typedef struct GLNode{
-    ElemTag tag;
-    union{
-        AtomType atom;
-        struct{
-            struct GLNode *hp, *tp;
-        }ptr;
-    };
-}*GList;
+#define MAX_ARRRAY_DIM 8    //设置数组维数的最大值
+typedef int ElemType;       
+typedef struct {
+    ElemType *base;     //数组元素基址,由InitArray分配
+    int     dim;        //数组维数
+    int     *bounds;    //数组维届基址,由InitArray分配
+    int     *constants;     //数组函数映像常量基址,由InitArray分配
+}Array;
